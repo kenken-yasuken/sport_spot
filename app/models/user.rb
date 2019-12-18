89bf1b213968d4dc57590fe58a1ceb90c68af809
kenken-yasuken <kenken.yasuken@gmail.com>
2019-12-19 00:18:16 +0900

    ユーザー詳細画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+# family_name                  string, null: false
+# first_name                   string, null: false
+# family_name_kana             string
+# first_name_kana              string
+# nickname                     string, null: false
+# gender                       int, null: false
+# age                          int,  null: false
+# birthday                     datetime
+# user_type_id                 int, null: false
+# encrypted_password           string, null: false
+
   enum gender: { male: 1, female: 2 }
   enum user_type_id: { trainer: 1, trainee: 2 }
 end
