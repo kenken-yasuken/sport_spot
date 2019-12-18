f2e65cf2469bdab4bf630faf45ef97c3e02ff901
kenken-yasuken <kenken.yasuken@gmail.com>
2019-12-19 00:20:14 +0900

    ユーザー詳細画面の作成 (#8)

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,9 +1,7 @@
 class Sport < ApplicationRecord
-  ##################################################
   # id                                    int
   # sport_category_id                     int
   # name                                  string
   # created_at                            datetime
   # updated_at                            datetime
-  ###################################################
 end
