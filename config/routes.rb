0c49729710946ff79985e1ac87f90dbcd97a3037
kenken-yasuken <kenken.yasuken@gmail.com>
2020-01-12 21:13:56 +0900

    devise関連view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'home/index'
   get 'home/show'
   root to: "home#index"
-
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
   resources :users
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
+  end
 end
